25b718fb29f8db6faa14c3dfdf8172055c976645
MohamedGoush <0x1110111@gmail.com>
2023-10-29 16:30:16 +0530

    Initial Commit

---
+++ Hue Rotation/Hue_RotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_RotationApp.swift
+//  Hue Rotation
+//
+//  Created by python on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Hue_RotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
